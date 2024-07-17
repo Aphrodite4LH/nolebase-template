46f8b56efc170526826aff86e65be7025e7bc1b6
UrThePumpkin <353152883@qq.com>
2024-07-17 22:41:44 +0800

    test

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,13 +1,13 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = 'pumpkin'
+export const siteShortName = 'pumpkin'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
-/** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+    /** Repo */
+    export const githubRepoLink = 'https://github.com/nolebase/nolebase'
 /** Discord */
 export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
